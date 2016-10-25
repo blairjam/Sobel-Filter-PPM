3a2ffd028b8de663343c41bfef55c76d71e6b6b7
James Blair <james.con.blair@hpe.com>
2016-10-25 10:27:04 -0500

    Running each version 5 times.

--- EdgeDetector/main.cpp
+++ EdgeDetector/main.cpp
@@ -6,54 +6,87 @@
 
 int main()
 {   
+	std::string dir = "./images/";
 	std::string name = "lady";
 	std::string type = ".ppm";
 
     /* READ */
-    std::cout << "Reading image." << std::endl;
-
-    auto image = read(name + type);
+	std::cout << "Reading image... ";
+    auto image = read(dir + name + type);
     if (image == nullptr)
     {
+		system("pause");
         return 1;
     }
-
-    std::cout << "Image read successful." << std::endl;
+    std::cout << "Done." << std::endl;
 
     /* GRAY */
-
-    std::cout << "Converting to grayscale." << std::endl;
-
+    std::cout << "Converting to grayscale... ";
     to_grayscale(image);
+    std::cout << "Done." << std::endl;
 
-    std::cout << "Grayscale conversion complete." << std::endl;
+	/* SERIAL FILTER */
+	std::cout << "\nApplying edge detection serially 5 times..." << std::endl;
 
-    /* WRITE */
+	std::vector<std::shared_ptr<PPMImage>> serial_images;
+	std::vector<double> serial_times;
 
-    std::cout << "Writing grayscale image to file." << std::endl;
+	for (int i = 0; i < 5; i++)
+	{
+		Timer timer;
 
-   // write(image, name + "_gray" + type);
+		timer.start();
+		auto img = apply_filter_serial(image);
+		timer.stop();
 
-    std::cout << "Image write successful." << std::endl;
+		serial_images.push_back(img);
+		serial_times.push_back(timer.get_elapsed_time());
+	}
 
-    /* FILTER */
+	write(serial_images[0], dir + name + "_serial" + type);
 
-    std::cout << "Applying edge detection filter." << std::endl;
+	double serial_tot = 0;
+	for (int i = 0; i < 5; i++)
+	{
+		serial_tot += serial_times[i];
+	}
 
-	Timer filter_timer;
-	filter_timer.start();
-    auto filtered = apply_filter_parallel(image);
-	filter_timer.stop();
+	double serial_avg = serial_tot / 5.0;
 
-    std::cout << "Filter application successful. Time: " << filter_timer.get_elapsed_time() << std::endl;
+	std::cout << "Done. Average runtime for edge detection: " << serial_avg << " seconds." << std::endl;
 
-    /* WRITE */
+	/* PARALLEL FILTER */
+	std::cout << "\nApplying edge detection in parallel 5 times..." << std::endl;
 
-    std::cout << "Writing image to file." << std::endl;
+	std::vector<std::shared_ptr<PPMImage>> para_images;
+	std::vector<double> para_times;
 
-    write(filtered, name + "_filter_para" + type);
+	for (int i = 0; i < 5; i++)
+	{
+		Timer timer;
 
-    std::cout << "Image write successful." << std::endl;
+		timer.start();
+		auto img = apply_filter_parallel(image);
+		timer.stop();
+
+		para_images.push_back(img);
+		para_times.push_back(timer.get_elapsed_time());
+	}
+
+	write(para_images[0], dir + name + "_para" + type);
+
+	double para_tot = 0;
+	for (int i = 0; i < 5; i++)
+	{
+		para_tot += para_times[i];
+	}
+
+	double para_avg = para_tot / 5.0;
+
+	std::cout << "Done. Average runtime for edge detection: " << para_avg << " seconds." << std::endl;
+
+
+	std::cout << "\nSpeedup of edge detection using 4 threads: " << (serial_avg / para_avg) << std::endl;
 
     system("pause");
     return 0;
